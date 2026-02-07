1e20914c8d7af9857e504655469dd70203bb8053
ThevidSoz <souzapx.david@gmail.com>
2026-02-07 12:55:31 -0300

    feat: Exercicio 2 - par ou impar

--- secao05/Exe_2.java
+++ secao05/Exe_2.java
@@ -1,5 +1,20 @@
-package java_exercicios_nelio_alves.secao_05;
+package java_exercicios_nelio_alves.secao05;
+
+import java.util.Scanner;
 
 public class Exe_2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Digite um número: ");
+        int numInput = sc.nextInt();
+
+        if (numInput % 2 == 0) {
+            System.out.printf("O número %d é PAR!", numInput);
+        } else {
+            System.out.printf("O número %d é IMPAR!", numInput);
+        }
+
+        sc.close();
+    }
 }
